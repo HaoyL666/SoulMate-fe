85f8d6c0c5570fdaf8b0dfa49b39cfeca9651f96
Haoyu <liu_haoyu@berkeley.edu>
2023-07-11 00:35:18 -0700

    create react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+//import { createRoot } from 'react-dom/client';
+//import { Provider } from 'react-redux';
+import App from './App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+);
